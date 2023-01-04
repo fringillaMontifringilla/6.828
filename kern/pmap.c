4502fe6b8ec34e89b295aa63d0b9f26cda13e706
6607changchun <luobojiaozi@163.com>
2023-01-04 11:18:53 +0800

    feat(lab2):kernel virtmem

--- kern/pmap.c
+++ kern/pmap.c
@@ -171,7 +171,14 @@ mem_init(void)
 	//    - the new image at UPAGES -- kernel R, user R
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
-	// Your code goes here:
+    boot_map_region(kern_pgdir, UPAGES,
+                    (int)ROUNDUP(npages*sizeof(struct PageInfo), PGSIZE),
+                    PADDR(pages),
+                    PTE_U);
+    boot_map_region(kern_pgdir, pages,
+                    (int)ROUNDUP(npages*sizeof(struct PageInfo), PGSIZE),
+                    PADDR(pages),
+                    PTE_W);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -183,7 +190,7 @@ mem_init(void)
 	//       the kernel overflows its stack, it will fault rather than
 	//       overwrite memory.  Known as a "guard page".
 	//     Permissions: kernel RW, user NONE
-	// Your code goes here:
+    boot_map_region(kern_pgdir, KSTACKTOP-KSTKSIZE, KSTKSIZE, PADDR(bootstack), PTE_W);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
@@ -192,7 +199,7 @@ mem_init(void)
 	// We might not have 2^32 - KERNBASE bytes of physical memory, but
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
-	// Your code goes here:
+    boot_map_region(kern_pgdir, KERNBASE, 0xffffffff-KERNBASE+1, 0, PTE_W);
 
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
@@ -377,7 +384,7 @@ boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm
 {
     for(size_t i = 0; i < size; i += PGSIZE){
         pte_t* pte = pgdir_walk(pgdir, (void*)(va + i), 1);
-        *pte = (PGNUM(pa + i) << PGSIZE) | perm | PTE_P;
+        *pte = (PGNUM(pa + i) << PGSHIFT) | perm | PTE_P;
     }
 }
 
