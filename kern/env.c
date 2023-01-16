05144c42ad6324c6c3b5e4799601096eaf5a9413
6607changchun <luobojiaozi@163.com>
2023-01-16 16:58:27 +0800

    feat(lab5):create fs env

--- kern/env.c
+++ kern/env.c
@@ -395,7 +395,8 @@ env_create(uint8_t *binary, enum EnvType type)
         panic("create env failed\n");
 
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
-	// LAB 5: Your code here.
+    if(type == ENV_TYPE_FS)
+        e -> env_tf.tf_eflags |= FL_IOPL_3;
 }
 
 //
