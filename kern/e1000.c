d1740e15b6dfa48b5f0efc51144615f9c0af3671
6607changchun <luobojiaozi@163.com>
2023-01-27 12:04:07 +0800

    feat(lab6):enable e1000 nic

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,4 +1,12 @@
+#include "pci.h"
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
-// LAB 6: Your driver code here
+#define COMMAND_INIT 0x0000
+#define STATUS_INIT 0x0230
+#define CLASS_CODE 0x020000
+#define REVID 0x00
+int e1000_82540em_attach(struct pci_func* pcif){
+    pci_func_enable(pcif);
+    return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,12 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 #endif  // SOL >= 6
+
+#include <kern/pci.h>
+#define PCI_82540EM_VENDOR 0x8086
+#define PCI_82540EM_DESKTOP_DEVICE 0x100e
+#define PCI_82540EM_MOBILE_DEVICE 0X1015
+
+#define PCI_82540EM_DESKTOP_ATTACH \
+    { PCI_82540EM_VENDOR, PCI_82540EM_DESKTOP_DEVICE, e1000_82540em_attach }
+int e1000_82540em_attach(struct pci_func* pcif);
