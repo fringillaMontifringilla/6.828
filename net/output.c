a5c91b52148a8b56c489b6bbb0bad2f924fbb813
6607changchun <luobojiaozi@163.com>
2023-01-28 11:06:10 +0800

    feat(lab6):add output helper env

--- net/output.c
+++ net/output.c
@@ -7,7 +7,22 @@ output(envid_t ns_envid)
 {
 	binaryname = "ns_output";
 
-	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+    while(1){
+        envid_t from_envid;
+        int32_t result = ipc_recv(&from_envid, &nsipcbuf, 0);
+        if(result < 0)
+            return;
+        if(from_envid != ns_envid)
+            return;
+        //now receive a packet
+        switch(result){
+            case NSREQ_OUTPUT:
+                int r = sys_nic_transmit(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len);
+                while(r)
+                    r = sys_nic_transmit(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len);
+                break;
+        }
+    }
 }
